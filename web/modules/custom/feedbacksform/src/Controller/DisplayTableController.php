1213234db6d537dd3301401a1c4a8be9afb96bad
quadrexdd <y.kozak@internetdevels.net>
2021-07-01 12:18:10 +0300

    WIP images displaying

--- web/modules/custom/feedbacksform/src/Controller/DisplayTableController.php
+++ web/modules/custom/feedbacksform/src/Controller/DisplayTableController.php
@@ -3,7 +3,7 @@
 namespace Drupal\feedbacksform\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-
+use Drupal\file\Entity\File;
 /**
  * Class DisplayTableController
  * @package Drupal\mymodule\Controller
@@ -16,13 +16,16 @@ class DisplayTableController extends ControllerBase
     //create table header
     $header_table = array(
       'id' => t('ID'),
-      'first_name' => t('first name'),
+      'first_name' => t('First name'),
       'email' => t('Email'),
-      'phone' => t('phone'),
+      'phone' => t('Phone'),
+      'feedback' => t('Feedback text'),
       'date' => t('Submit date'),
+      'avatar' => t('Avatar image'),
+      'feedback_image' => t ('Feedback image'),
     );
     $query = \Drupal::database()->select('feedbacks', 'm');
-    $query->fields('m', ['id', 'first_name', 'email_address', 'phone_number', 'submit_date']);
+    $query->fields('m', ['id', 'first_name', 'email_address', 'phone_number', 'feedback', 'submit_date', 'fid_avatar_image', 'fid_feedback_image']);
     $results = $query->execute()->fetchAll();
     $rows = array();
     foreach ($results as $data) {
@@ -33,7 +36,10 @@ class DisplayTableController extends ControllerBase
         'first_name' => $data->first_name,
         'email' => $data->email_address,
         'phone_number' => $data->phone_number,
+        'feedback' => $data->feedback,
         'submit_date' => $data->submit_date,
+        'avatar_image'=> $data->fid_avatar_image,
+        'feedback_image'=>$data->fid_feedback_image,
       );
 
     }
@@ -42,7 +48,7 @@ class DisplayTableController extends ControllerBase
       '#type' => 'table',
       '#header' => $header_table,
       '#rows' => $rows,
-      '#empty' => t('No data found, sorry nigga!'),
+      '#empty' => t('No data found, sorry bro!'),
     ];
     return $form;
   }
