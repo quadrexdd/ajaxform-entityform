56418e2b489acb3423cdade69b7a4f138b955a59
quadrexdd <y.kozak@internetdevels.net>
2021-07-05 11:05:22 +0300

    WIP ajax validation

--- web/modules/custom/feedbacksform/src/Form/AjaxFormSubmit.php
+++ web/modules/custom/feedbacksform/src/Form/AjaxFormSubmit.php
@@ -6,6 +6,10 @@
 
 namespace Drupal\feedbacksform\Form;
 
+use Drupal\Core\Ajax\AddCssCommand;
+use Drupal\Core\Ajax\AjaxResponse;
+use Drupal\Core\Ajax\CssCommand;
+use Drupal\Core\Ajax\HtmlCommand;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Utility\Xss;
@@ -30,12 +34,30 @@ class AjaxFormSubmit extends FormBase {
       '#desctiption' => $this->t('Enter your First name.'),
       '#required' => TRUE,
       '#allowed_tags' => Xss::getHtmlTagList(),
+      '#ajax' => [
+        'callback' => '::validateFirstnameAjax',
+        'event' => 'change',
+        'progress' => array(
+          'type' => 'throbber',
+          'message' => t('Verifying first name..'),
+        ),
+      ],
+      '#suffix' => '<div class="firstname-validation-message"></div>'
     ];
     $form['email_address'] = [
       '#type' => 'email',
       '#title' => $this->t('E-mail'),
       '#description' => $this->t('Please enter your e-mail address'),
       '#required' => TRUE,
+      '#ajax' => [
+        'callback' => '::validateEmailAjax',
+        'event' => 'change',
+        'progress' => array(
+          'type' => 'throbber',
+          'message' => t('Verifying email..'),
+        ),
+      ],
+      '#suffix' => '<div class="email-validation-message"></div>'
     ];
     $form['phone_number'] = [
       '#type' => 'tel',
@@ -43,6 +65,15 @@ class AjaxFormSubmit extends FormBase {
       '#description' => $this->t('Please, enter your phone number'),
       '#required' => TRUE,
       '#maxlength' => '10',
+      '#ajax' => [
+        'callback' => '::validatePhoneAjax',
+        'event' => 'change',
+        'progress' => array(
+          'type' => 'throbber',
+          'message' => t('Verifying email..'),
+        ),
+      ],
+      '#suffix' => '<div class="phone-validation-message"></div>'
     ];
     $form['feedback'] = [
       '#type' => 'textfield',
@@ -81,11 +112,43 @@ class AjaxFormSubmit extends FormBase {
     return $form;
   }
 
-  public function validateForm(array &$form, FormStateInterface $form_state) {
+  public function validateFirstnameAjax (array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
     $first_name=$form_state->getValue('first_name');
-    if(strlen($first_name) <=2 && strlen($first_name)>= 100) {
-      $form_state->setErrorByName('first_name', 'Enter correct first name');
+    if (strlen($first_name)<=2 || ($first_name) >=100) {
+      $response->addCommand(new HtmlCommand('.firstname-validation-message', 'First name should contain >2 and <100 characters'));
     }
+    else {
+      $response->addCommand(new HtmlCommand('.firstname-validation-message', ''));
+    }
+    return $response;
+  }
+  public function validateEmailAjax (array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $email_address = $form_state->getValue('email_address');
+    $pattern_mail = preg_match("/[a-zA-Z0-9 +-]+@[a-zA-Z0-9 +-]+\.[a-zA-Z0-9]+/", $email_address);
+    if (!$pattern_mail) {
+      $response->addCommand(new HtmlCommand('.email-validation-message', 'Fuck, your email is not valid!'));
+    }
+    else {
+      $response->addCommand(new HtmlCommand('.email-validation-message', ''));
+    }
+    return $response;
+  }
+  public function validatePhoneAjax (array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+    $phone_number = $form_state->getValue('phone_number');
+    $pattern_phone = preg_match("/^[0-9]{10}$/", $phone_number);
+    if (!$pattern_phone) {
+      $response->addCommand(new HtmlCommand('.phone-validation-message', 'Fuck, your phone is not valid!'));
+    }
+    else {
+      $response->addCommand(new HtmlCommand('.phone-validation-message', ''));
+    }
+    return $response;
+  }
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+
   }
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $get_current_time=\Drupal::time()->getCurrentTime();
